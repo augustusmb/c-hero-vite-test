5375bbf6cd125d0c53dca8f4c1f5c8df5e82bd05
Augustus Brennan <augustusmb@gmail.com>
2023-11-14 19:11:58 -0800

    update server unneeded code

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ import express from 'express';
 dotenv.config()
 const __dirname = path.resolve();
 
-
 import { defineConfig, loadEnv } from 'vite';
 
 const env = loadEnv(
@@ -23,53 +22,14 @@ app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
 const port = env.PORT || 8080
 
-
-
-
-/**  This is an example basic db query  **/
-// db.query('SELECT * FROM users')
-//   .then(data => {
-
-//   })
-//   .catch(err => {
-//   })
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// if (import.meta.env.VITE_NODE_ENV === "production") {
-//   app.use(express.static("dist"));
-// } else {
-//   app.use(express.static(path.join(__dirname, 'public')));
-// }
-
 app.use('/routes', router)
 
-app.get('/test', (req, res) => {
-  db.query('select * from users')
-  .then(res => {
-    console.log(res)
-  })
-  // console.log(req)
-  console.log('test')
-  // console.log(req.query)
-  res.send('hello')
-})
-
-// app.post('/testRoute', (req, res) => {
-//   console.log('The test endpoint has been reached. Congratulations')
-//   console.log('Request here ' + req.body)
-//   res.send('Good Job')
-// })
-
 if (env.VITE_NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("public"));
 }
 
 app.get('/', (req, res) => {
   console.log("Hello")
-
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
